972738eb5feff656a94a3995d1853824c863484d
mk6139 <133147321+mk6139@users.noreply.github.com>
2023-05-10 20:22:47 +0200

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
+
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
 
 const App = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const goToNextTrack = () => {
+    setTrackIndex(trackIndex + 1);
+  };
+
+  const { data: tracks } = useQuery({
+		queryKey: ['tracks'],
+		queryFn: fetchTracks
+});
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +32,10 @@ const App = () => {
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
       <div className="App-buttons"></div>
+
+      <audio src={trackUrls[trackIndex]} autoPlay controls />
+
+      <button onClick={goToNextTrack}>Next track</button>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,17 @@
-const apiToken: string = '';
+const apiToken: string =
+  'BQA8ksh4xm5jXF9mBlH9Kaf9waZ_WX7lCw1LwO27sHVsF_Zuoq0uzsR1FS1XeYopWFm7gGrwCe03KrGNO4LiGwYgjvyxXRISVNALnkyz_D8A1Q7vYh4B9ZdJJgXWsoz9D2CunsHR1uid5c6Qt7go1Vu-A6oUEHfH4O-o1B0fujeU0EcLynh3M-6IbvitVCZeFiMif7618va5bxlHs-k8PZE';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: unknown[] };
+
+  return data.items;
 };
